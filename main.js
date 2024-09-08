f32a6b94f86d45b07153e1bfb9638bb8050873b3
Alina <ms.sciurus@gmail.com>
2024-09-08 12:58:18 +0300

    add some styles and functions

--- main.js
+++ main.js
@@ -1,1 +1,62 @@
-console.log('Hello There!');
+const userNumberOutput = document.querySelector('.field-form-input'),
+      checkButton = document.querySelector('.field-form-btn'),
+      newNumberButton = document.querySelector('.field-again-btn'),
+      resultOutput = document.querySelector('.result-check-out'),
+      clueOutput = document.querySelector('.result-help-out'),
+      attemptsOutput = document.querySelector('.result-count-out');
+
+// Function for create random number
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+}
+
+const computerNumber = getRandomNumber();
+
+let clickCount = 0;
+
+//user output function
+
+function compareNumbers() {
+    console.log(computerNumber);
+    attemptsOutput.innerHTML = clickCount;
+
+    checkButton.addEventListener('click', () => {
+        if(!userNumberOutput.value) {
+            return clueOutput.innerHTML = 'Add some number please ^•-•^';
+        }
+
+        if((Number(userNumberOutput.value) > 100) || (Number(userNumberOutput.value) < 0)) {
+            return clueOutput.innerHTML = "That wasn't a deal :(";
+        }
+
+        if(computerNumber !== Number(userNumberOutput.value)) {
+            clickCount++
+        }
+
+        if (computerNumber < Number(userNumberOutput.value))  {
+            console.log('many');
+            clueOutput.innerHTML = 'Many';
+        } else if (computerNumber > Number(userNumberOutput.value)) {
+            console.log('low');
+            clueOutput.innerHTML = 'Low';
+        } else if (computerNumber == Number(userNumberOutput.value)) {
+            console.log('good');
+            clueOutput.innerHTML = 'Good!';
+            resultOutput.innerHTML = 'Congratulations! You guessed the number!';
+        }
+
+        return attemptsOutput.innerHTML = clickCount;
+    })
+}
+
+compareNumbers();
+
+function newGame() {
+    newNumberButton.addEventListener('click', () => {
+        console.log(getRandomNumber());
+        
+    });
+}
+
+newGame();
